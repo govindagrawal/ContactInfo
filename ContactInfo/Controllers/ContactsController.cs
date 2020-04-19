088013c2fe885a1bc539a275f2d3e9562c91ee38
Govind Agrawal <govind.agrawal1@wipro.com>
2020-04-19 14:07:16 +0530

    Initial Setup

---
+++ ContactInfo/Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactInfo.Models;
+
+namespace ContactInfo.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ViewResult Index()
+        {
+            var contacts = GetContacts();
+            return View(contacts);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var contact = GetContacts().SingleOrDefault(c => c.Id == id);
+
+            if (contact == null)
+                return HttpNotFound();
+
+            return View(contact);
+        }
+
+        private IEnumerable<Contact> GetContacts()
+        {
+            return new List<Contact>();
+        }
+    }
+}
